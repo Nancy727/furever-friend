c34774a7033d15b5a36e5ba40b8cc7df6e6b2372
Nancy Kumari <151949774+Nancy727@users.noreply.github.com>
2024-06-16 18:52:04 +0530

    Add files via upload

--- homeScript.js
+++ homeScript.js
@@ -1,2 +1,2 @@
 let full_name=prompt("Enter Your Full Name");
-alert(`"Welcome to Pet_Finder,${full_name}! We're delighted to have you here. Explore our furry friends awaiting loving homes, and we hope you find a perfect companion who will bring joy and love into your life."`);
+alert(`"Welcome to Furever_Friend,${full_name}! We're delighted to have you here. Explore our furry friends awaiting loving homes, and we hope you find a perfect companion who will bring joy and love into your life."`);
